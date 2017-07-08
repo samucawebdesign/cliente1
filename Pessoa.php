8fe85fc6d99d38bca5cb0ae34c0e36ca0083844a
Samuca Webdesign <samuca@samuca.eti.br>
2017-07-08 14:59:16 -0300

    Primeiro salvamento

---
+++ Pessoa.php
@@ -0,0 +1,38 @@
+﻿<?PHP  
+class Pessoa
+	{
+		private $nome;
+		private $idade;
+		private $sexo;
+		
+		public function fazerAniversario()
+			{
+				$this->idada++;
+			}
+			
+		public function getNome(){
+			return $this->nome;
+		}
+	
+		public function setNome($nome){
+			$this->nome = $nome;
+		}
+	
+		public function getIdade(){
+			return $this->idade;
+		}
+	
+		public function setIdade($idade){
+			$this->idade = $idade;
+		}
+	
+		public function getSexo(){
+			return $this->sexo;
+		}
+	
+		public function setSexo($sexo){
+			$this->sexo = $sexo;
+		}
+		
+	}
+?>
